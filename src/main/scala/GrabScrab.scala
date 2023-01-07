75e4e6077344113c60aeef05085494d58e05135e
Mikolaj Kania <mikolaj.kania@freshmail.pl>
2023-01-07 14:07:15 +0100

    GrabScrab

---
+++ src/main/scala/GrabScrab.scala
@@ -0,0 +1,8 @@
+// 6 kyu
+// https://www.codewars.com/kata/52b305bec65ea40fe90007a7/train/scala
+
+object GrabScrab {
+
+  def grabscrab(word: String, possibleWords: Seq[String]): Seq[String] =
+    possibleWords.filter(_.sorted.equals(word.sorted))
+}
